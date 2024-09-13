a3532717f76b7da1fae12620d045c2fda363a143
Glenn Torrens <glenntorrens@gmail.com>
2024-09-13 14:34:39 +0900

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,11 @@
-// import { OrbitControls } from '@react-three/drei';
-// import { Canvas, useFrame } from '@react-three/fiber';
-// import { useRef } from 'react';
+import React from 'react'
 
-// const RotatingCube = () => {
-//   const meshRef = useRef();
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+        <h1 className="text-2xl text-white underline">Sup, skanks.</h1>
+    </main>
+  )
+}
 
-//   useFrame(() => {
-//     if(meshRef.current) {
-//       meshRef.current.rotation.x += 0.01
-//       meshRef.current.rotation.y += 0.01
-//     }
-//   })
-
-//   return (
-//     <mesh ref={meshRef} position={[0, 0, 0]}>
-//       <boxGeometry />
-//       <meshStandardMaterial color={0x5F9EA0} emissive={0x5F9EA0} />
-//     </mesh>
-//   )
-// }
-// const App = () => {
-//   return (
-//     <Canvas style={{ width: '100vw', height: '100vh' ,display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
-//       <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
-
-//       <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
-
-//       <color attach="background" args={['#F0F0F0']} /> 
-//       {/* args is an array and as a hexidecimal color is stored within a string */}
-
-//       <RotatingCube />
-//       {/* this cube is declared above */}
-//     </Canvas>
-//   )
-// }
-
-// export default App;
-// the above code is used to create a rotating cube and it is rendered in the canvas and it is animated using the useFrame hook
+export default App
