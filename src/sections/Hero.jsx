fb73efe1fafefe8a9bc83395f412b6140a842979
Glenn Torrens <glenntorrens@gmail.com>
2025-01-06 23:07:57 +0900

    restarting hero and hacker room

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,32 +1,24 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
-import HackerRoom from "../components/HackerRoom";
-import CanvasLoader from "../components/CanvasLoader";
+import { PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import HackerRoom from '../components/HackerRoom'
 
 const Hero = () => {
   return (
-    <section className="min-h-screen w-full flex flex-col relative">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space sm:px-10 px-5 gap-3">
-        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-          Hey, I am Glenn <span className="waving-hand">✌️</span>a Software
-          Engineer from NYC<span className="waving-hand">🗽</span> in JPN{" "}
-          <span className="waving-hand">🎌</span>{" "}
-        </p>
-        <p className="hero_tag text-gray_gradient text-sm">
-          I Make Awesome Things!<span className="waving-hand">🤩</span>{" "}!
-        </p>
-      </div>
-      <div className="w-full h-full absolute inset-0">
-        <Canvas className="w-full h-full">
-          <Suspense fallback={<CanvasLoader />}>
+    <section className='min-h-screen w-full flex flex-col relative'>
+      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Glenn <span className='waving-hand'>✌️</span></p>
+        <p className='hero_tag text-gray_gradient'>Building Ideas and Bringing them to Life <span className='waving-hand'>🧙🏾‍♂️</span></p>
+        <div className='w-full h-full absolute inset-0'>
+          <Canvas className='w-full h-full'>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-            <HackerRoom />
-          </Suspense>
-        </Canvas>
-      </div>
-    </section>
-  );
-};
 
-export default Hero;
+            <HackerRoom />
+          </Canvas>
+        </div>
+      </div>
+      </section>
+  )
+}
+
+export default Hero
