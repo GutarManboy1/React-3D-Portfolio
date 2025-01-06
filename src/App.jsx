9ef53087e1abe5e4117854da27934da0c6eb9303
Glenn Torrens <glenntorrens@gmail.com>
2025-01-06 18:54:04 +0900

    refreshing the whole kit and kaboodle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
-import Navbar from './Sections/Navbar'
-import Hero from './Sections/Hero'
-// import HackerRoom from './components/HackerRoom'
+import React from 'react'
+import Navbar from './sections/Navbar'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
-      <Navbar />
-      <Hero />
-      {/* <HackerRoom /> */}
-    </main>
+        <Navbar/>
+      </main>
   )
 }
 
